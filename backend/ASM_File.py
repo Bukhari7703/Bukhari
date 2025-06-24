2e26762999032a95b2204f7f338e938762ae29d8
Bukhari <rashidulbukhari12@gmail.com>
2025-06-25 03:54:51 +0800

    Latest

--- backend/ASM_File.py
+++ backend/ASM_File.py
@@ -20,9 +20,9 @@ y_scaler = joblib.load(os.path.join(BASE_DR, 'y_scaler.pkl'))
 
 '''Initialising Firebase '''
 # .env files holds the credentials for local hosting
-env_path = Path(__file__).resolve().parent / ".env"
-load_dotenv(dotenv_path=env_path)
-# load_dotenv()
+# env_path = Path(__file__).resolve().parent / ".env"
+# load_dotenv(dotenv_path=env_path)
+load_dotenv()
 
 firebase_config = {
     "type": os.getenv("FIREBASE_TYPE"),
